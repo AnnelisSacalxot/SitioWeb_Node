1c48f60e9c11298b2927b67deb902afd4a41c6ba
Annelís <juanysacalxot376@gmail.com>
2021-12-18 21:19:28 -0600

    Corriendo ahora el archivo index.ejs indicandole que se usara ese motor de plantilla para luego convertirlo en html

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const path = require('path');
 //si se desea modificar el puerto del servidor se manejara
 // con una constante
 app.set('port', 3000);
-//app.set('views', path.join(__dirname,'views'));//dirname: dar la direccion del archivo
-//app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname,'views'));//dirname: dar la direccion del archivo
+app.set('view engine' , 'ejs');
 
 //middlewares
 
 //routes
 app.get('/',(req, res) =>{
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-    //res.render('index');
+   // res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index');
 });
 
 //static files
