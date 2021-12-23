817b4dcd06d71a027f7c519e385f22461d66bf2d
Annelís <juanysacalxot376@gmail.com>
2021-12-23 00:25:38 -0600

    Utilizacion de Docker para poder correr la imagen deseada

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router(); 
 
 router.get('/',(req, res) =>{
-    // res.sendFile(path.join(__dirname, 'views/index.html'));
      res.render('laboratorista', {title: 'Datos examen'});
  });
 
